b393ac10d09d9c57ebb3ef5bef54591fff56cc24
lyklecharova <lklecharova9@gmail.com>
2024-04-17 18:36:37 +0300

    Create Footer.jsx

---
+++ Aquarium-fish-website/client/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import style from './Footer.module.css'
+export const Footer = () => {
+  return (
+    <footer className={style['footer']}>
+      <p>All rights reserved</p>
+    </footer>
+  );
+};
