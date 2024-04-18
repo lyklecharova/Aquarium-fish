04d6188a4cfeb016b8aba9a198749e3aa8578e34
lyklecharova <lklecharova9@gmail.com>
2024-04-18 07:33:28 +0300

    Update PageNotFound.jsx

--- Aquarium-fish-website/client/src/components/PageNotFound/PageNotFound.jsx
+++ Aquarium-fish-website/client/src/components/PageNotFound/PageNotFound.jsx
@@ -2,7 +2,7 @@ import style from './PageNotFound.module.css';
 export const PageNotFound = () => {
     return (
         <div className={style['error']}>
-            <img className={style['error-image']} src="/public/image/vecteezy_illustration-of-404-page-not-found_7711625.jpg" alt="error-page" />
+            <img className={style['error-image']} src="/image/vecteezy_illustration-of-404-page-not-found_7711625.jpg" alt="error-page" />
         </div>
     );
 };
