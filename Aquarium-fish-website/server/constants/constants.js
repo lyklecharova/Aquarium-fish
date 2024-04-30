3de504313c718222f58674f63e52b1fbef7b979e
lyklecharova <lklecharova9@gmail.com>
2024-04-30 13:20:34 +0300

    Create constants.js

---
+++ Aquarium-fish-website/server/constants/constants.js
@@ -0,0 +1,9 @@
+const JWT_SECRET = '5e31bd17-d124-4e2f-8943-36fb696a9f28';
+const PORT = 3030;
+const MONGO_DB_CONNECTION_STRING = 'mongodb://127.0.0.1:27017/aquarimum-fish';
+
+module.exports = {
+    JWT_SECRET,
+    PORT,
+    MONGO_DB_CONNECTION_STRING,
+}
