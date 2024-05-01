64ed36e5ba6431e45e6e82472032b3ab6abae530
lyklecharova <lklecharova9@gmail.com>
2024-05-01 07:55:23 +0300

    Create Edit.jsx

---
+++ Aquarium-fish-website/client/src/components/Edit/Edit.jsx
@@ -0,0 +1,154 @@
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import * as FishService from '../../../service/FishService';
+import style from './Edit.module.css';
+
+export const Edit = () => {
+    const nav = useNavigate();
+
+    const { fishId } = useParams();
+    const [fish, setFish] = useState({
+        species: '',
+        image: null,
+        description: '',
+    });
+
+    const [fishError, setFishError] = useState({});
+
+    //Mount
+    useEffect(() => {
+        FishService.getOneFish(fishId)
+            .then(result => {
+                setFish(result);
+            });
+    }, [fishId]);
+
+
+    const handleEditSubmit = async (e) => {
+        e.preventDefault();
+        const fishErr = {};
+        if (fish.species.trim() === '') {
+            fishErr.speciesError = 'You should enter species';
+        }
+        if (e.currentTarget.image.files.length === 0) {
+            fishErr.imageError = 'You should upload image';
+        }
+        if (fish.description.trim() === '') {
+            fishErr.descriptionError = 'You should enter description'
+        }
+
+        setFishError(fishErr);
+
+        if (Object.keys(fishErr).length === 0) {
+            const editedFish = {
+                species: e.currentTarget.species.value,
+                image: URL.createObjectURL(e.currentTarget.image.files[0]),
+                description: e.currentTarget.description.value
+            };
+
+            if (!editedFish.image) {
+                alert("Please select an image");
+                return; // Излизаме от функцията или изпълнението на кода
+            }
+
+            try {
+                await FishService.editFish(fish, editedFish);
+                nav('/catalog');
+            } catch (error) {
+                console.log(`Please try again ${error}`);
+            }
+        }
+
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFish((fish) => ({
+            ...fish,
+            [name]: value,
+        }));
+    };
+
+    // Функцията handleImageChange се извиква при промяна на изображението в инпут полето.
+    // При всяка промяна на изображението, извикваме FileReader, който ни позволява да
+    // прочетем съдържанието на файла като Data URL (base64 закодиран стринг).
+    const handleImageChange = (e) => {
+        // Взимаме първият файл от списъка с файловете, който е избран от потребителя.
+        const file = e.target.files[0];
+        // Проверяваме дали файлът съществува
+        if (file) {
+            // Създаваме нов FileReader обект.
+            const reader = new FileReader();
+            // Използваме onload събитието, което се извиква, когато операцията на
+            // FileReader приключи успешно или неуспешно.
+            reader.onloadend = () => {
+                // При успешно приключване, в reader.result се съдържа съдържанието на файла
+                // като Data URL, което представлява файла като base64 закодиран стринг.
+
+                // Обновяваме състоянието на компонента, за да запишем изображението в него.
+                setFish((fish) => ({
+                    ...fish,
+                    image: reader.result,
+                }));
+            };
+            // Използваме readAsDataURL(), за да прочетем съдържанието на файла като Data URL.
+            reader.readAsDataURL(file);
+        }
+    };
+
+
+    return (
+        <div className={style['container']}>
+            <form onSubmit={handleEditSubmit} className={style['edit-form']} method="post">
+                <div className={style['form-group']}>
+                    <label htmlFor="species">Species</label>
+                    <input
+                        type="text"
+                        id="species"
+                        name="species"
+                        value={fish.species}
+                        onChange={handleInputChange}
+                        placeholder="Enter fish species..."
+                    />
+                </div>
+
+                {fishError.speciesError && (
+                    <p className={style['error']}>{fishError.speciesError}</p>
+                )}
+
+                <div className={style['form-group']}>
+                    <label htmlFor="image">Image</label>
+                    <input
+                        type="file"
+                        id="image"
+                        name="image"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                    />
+                </div>
+                {fishError.imageError && (
+                    <p className={style['error']}>{fishError.imageError}</p>
+                )}
+
+                <div className={style['form-group']}>
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                        id="description"
+                        name="description"
+                        value={fish.description}
+                        onChange={handleInputChange}
+                        rows="4"
+                        placeholder="Enter fish description..."
+                    ></textarea>
+                </div>
+                {fishError.descriptionError && (
+                    <p className={style['error']}>{fishError.descriptionError}</p>
+                )}
+
+                <button type="submit" className={style['edit-button']}>Edit</button>
+
+            </form>
+        </div>
+    );
+}
